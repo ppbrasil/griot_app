2123a2529ca3861e4e0a6b9e4292d778acd1d6c1
Pedro Brasil <ppbrasil@gmail.com>
2023-06-11 12:15:20 -0300

    Initial layout for the ButtomNavigationBar

--- lib/memories/presentation/pages/accounts_list_page.dart
+++ lib/memories/presentation/pages/accounts_list_page.dart
@@ -8,25 +8,79 @@ class AccountsListPage extends StatefulWidget {
 }
 
 class _AccountsListPageState extends State<AccountsListPage> {
+  int _currentIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        child: const Center(
-          child: Expanded(
-            flex: 1,
-            child: Text(
-              'Você está logado',
-              style: TextStyle(
-                color: Colors.green,
-                fontSize: 20,
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.white),
+          child: const Center(
+            child: Expanded(
+              flex: 1,
+              child: Text(
+                'Você está logado',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 20,
+                ),
               ),
             ),
           ),
         ),
+      ),
+      bottomNavigationBar: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          BottomNavigationBar(
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: '',
+                backgroundColor: Color.fromRGBO(81, 172, 135, 1),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: '',
+                backgroundColor: Color.fromRGBO(81, 172, 135, 1),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                label: '',
+                backgroundColor: Color.fromRGBO(81, 172, 135, 1),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera_alt_outlined),
+                label: '',
+                backgroundColor: Color.fromRGBO(81, 172, 135, 1),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: '',
+                backgroundColor: Color.fromRGBO(81, 172, 135, 1),
+              ),
+            ],
+          ),
+          Positioned(
+            top: -10,
+            left: MediaQuery.of(context).size.width / 2 - 30,
+            child: FloatingActionButton(
+              child: const Icon(Icons.add, color: Colors.white),
+              onPressed: () => {},
+            ),
+          )
+        ],
       ),
     );
   }
